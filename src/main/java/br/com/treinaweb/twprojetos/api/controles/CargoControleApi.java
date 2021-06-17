a56da8a0706c4701acd615c6bbfc81b441c81950
Cleyson Lima <cleysonph@gmail.com>
2021-06-16 23:08:18 -0300

    05:04 - criado a action de busca de cargos por id

--- src/main/java/br/com/treinaweb/twprojetos/api/controles/CargoControleApi.java
+++ src/main/java/br/com/treinaweb/twprojetos/api/controles/CargoControleApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class CargoControleApi {
     @GetMapping
     public List<Cargo> buscarTodos() {
         return cargoServico.buscarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cargo buscarPorId(@PathVariable Long id) {
+        return cargoServico.buscarPorId(id);
     }
 
 }
